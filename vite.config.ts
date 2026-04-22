18cc7027061ea925d4795a5c2d6391b352d9b85e
raine <raine.correa@wicomm.com.br>
2026-04-22 13:45:31 -0300

    feat(inMemory): adicionado mock test e correcao vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import { defineConfig } from "vitest/config";
-import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  resolve: {
+    tsconfigPaths: true,
+  },
+  test: {
+    environment: "node",
+    include: ["src/**/*.spec.ts", "src/**/*.test.ts"],
+  },
 });
