b22ace1dcdf7aa2ceb6b42c66d0b509f8eff34ca
raine <raine.correa@wicomm.com.br>
2026-04-22 13:33:18 -0300

    feat(test): test unitatio 01

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,5 +1,33 @@
-import { expect, test } from "vitest";
+import { expect, describe, it } from "vitest";
+import { RegisterUseCase } from "./register";
+import { compare } from "bcryptjs";
 
-test("check if it works", () => {
-  expect(2 + 2).toBe(4);
+describe("Register Use Case", () => {
+  it("should hash user password upon registration", async () => {
+    const registerUseCase = new RegisterUseCase({
+      findByEmail: async () => null,
+      async create(data) {
+        return {
+          id: "user-123",
+          name: data.name,
+          email: data.email,
+          password_hash: data.password_hash,
+          created_at: new Date(),
+        };
+      },
+    });
+
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "password123",
+    });
+
+    const isPasswordCorrectyHashed = await compare(
+      "password123",
+      user.password_hash,
+    );
+
+    expect(isPasswordCorrectyHashed).toBe(true);
+  });
 });
